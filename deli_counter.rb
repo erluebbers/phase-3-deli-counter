253b387ebe0734c0fe7e77c0412ae33fc6e0b55b
Erik Luebbers <erluebbers@gmail.com>
2022-02-08 07:14:28 -0800

    commit tree 6c3bc72cee889e76f83275e159222b5431dfab4b

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+require "pry"
+
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else 
+    name_list = katz_deli.map.with_index(1) { |name, idx| "#{idx}. #{name}" }
+    puts "The line is currently: #{name_list.join(" ")}"
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.index(name) + 1} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else 
+    puts "Currently serving #{katz_deli.shift()}."
+  end
+end
+
+
